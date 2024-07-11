aa89cf5db3a66b249a22b4f6531242d65798fdc5
czelabueno <c.zelabueno@gmail.com>
2024-07-11 00:35:15 -0400

    adding dashcope integration model

--- langchain4j-moa/src/test/java/dev/langchain4j/moa/MixtureOfAgentsIT.java
+++ langchain4j-moa/src/test/java/dev/langchain4j/moa/MixtureOfAgentsIT.java
@@ -1,9 +1,12 @@
 package dev.langchain4j.moa;
 
+import dev.langchain4j.model.anthropic.AnthropicChatModel;
 import dev.langchain4j.model.anthropic.AnthropicStreamingChatModel;
 import dev.langchain4j.moa.internal.DefaultMixtureOfAgents;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.dashscope.QwenModelName;
 import dev.langchain4j.model.mistralai.MistralAiChatModel;
 import dev.langchain4j.model.mistralai.MistralAiChatModelName;
 
@@ -19,31 +22,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MixtureOfAgentsIT {
 
     // Define reference models, it's recommended instance from 2 to 4 models
-    ChatLanguageModel qwen272B = MistralAiChatModel.builder()
-            .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
-            .modelName(MistralAiChatModelName.MISTRAL_LARGE_LATEST)
-            .temperature(0.7)
-            .build();
-
-    ChatLanguageModel qwen1572B = MistralAiChatModel.builder()
-            .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
-            .modelName(MistralAiChatModelName.OPEN_MISTRAL_7B)
-            .temperature(0.7)
-            .build();
-
     ChatLanguageModel openMixtral822B = MistralAiChatModel.builder()
-            .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
+            .apiKey(getenv("MISTRAL_AI_API_KEY"))
             .modelName(MistralAiChatModelName.OPEN_MIXTRAL_8X22B)
             .temperature(0.7)
             .build();
 
-    ChatLanguageModel claude3Haiku = MistralAiChatModel.builder()
-            .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
-            .modelName(MistralAiChatModelName.OPEN_MIXTRAL_8x7B)
-            .temperature(0.7)
+    ChatLanguageModel claude3Haiku = AnthropicChatModel.builder()
+            .apiKey(getenv("ANTHROPIC_API_KEY"))
             .build();
 
-    //List<ChatLanguageModel> refLlms = Arrays.asList(mistralLarge, qwen27B, qwen1572B, openaiGpt35);
+    ChatLanguageModel qwen1572B = QwenChatModel.builder()
+            .apiKey(getenv("DASHSCOPE_API_KEY"))
+            .modelName(QwenModelName.QWEN1_5_72B_CHAT)
+            .build();
+
+    ChatLanguageModel qwen272B = QwenChatModel.builder()
+            .apiKey(getenv("DASHSCOPE_API_KEY"))
+            .modelName(QwenModelName.QWEN2_72B_INSTRUCT)
+            .build();
+
     List<AgentChatLanguageModel> refLlms = Arrays.asList(
             AgentChatLanguageModel.from("openMixtral822B", openMixtral822B),
             AgentChatLanguageModel.from("claude3Haiku", claude3Haiku),
